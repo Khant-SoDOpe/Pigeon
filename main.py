e2d62f31d18160a8f7ff6454c0a6155d333e82b4
Khantzay2113 <khantzay.ly@gmail.com>
2023-05-28 09:03:39 +0630

    edit some link

--- main.py
+++ main.py
@@ -171,4 +171,4 @@ def final():
         return render_template("index.html",noti=noti)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=random.randint(2000,9000))
+    app.run(host='0.0.0.0',port=random.randint(2000,9000),debug=True)
